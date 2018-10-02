13fdc4ae120a5aab69d2bc3d8c161b3309784f84
Rodrigo <rodrigodot42@gmail.com>
2018-10-01 21:57:19 -0300

    Added Provider and combineReducers

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import { Provider } from 'react-redux'
+import {
+  combineReducers,
+  createStore
+} from 'redux'
+
 import ClassField from './field'
 
+const reducers = combineReducers({
+  field: () => ({ value: 'OPA' })
+})
+
 ReactDom.render(
-  <ClassField initialValue='Digite algo no input abaixo!'/>,
+  <Provider store={ createStore(reducers) }>
+    <ClassField initialValue='Digite algo no input abaixo!'/>
+  </Provider>,
   document.getElementById('app')
 )
